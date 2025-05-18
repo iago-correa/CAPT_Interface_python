28cfc931022d4b2d4ab30527a0398bf6334825af
Iago <iago.correa@outlook.com>
2025-05-18 23:40:43 +0900

    Record models

--- record/models.py
+++ record/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Experiment(models.Model):
+    session = models.ForeignKey(
+        "login.Session", 
+        on_delete=models.DO_NOTHING, 
+        related_name="experiments")
+    control = models.BooleanField(default=True) 
+
+class Recording(models.Model):
+    original_audio = models.ForeignKey(
+        "practice.Audio", 
+        on_delete=models.DO_NOTHING, 
+        related_name="recordings")
+    experiment= models.ForeignKey(
+        Experiment, 
+        on_delete=models.DO_NOTHING, 
+        related_name="recordings")
+    filename = models.CharField(max_length=255, unique=True)
