793b748c5dc48ce5db81e4c2573b69f3c6eaa307
Iago <iago.correa@outlook.com>
2025-05-19 00:27:23 +0900

    Login urls

--- login/views.py
+++ login/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    # return HttpResponse("Login view")
+    return render(request, 'login/login.html')
+
+def logout_view(request):
+    return HttpResponse("Logout view")
+    #return render(request, 'login/logout.html')
+
+def sessions_view(request, student_id):
+    return HttpResponse("%s" % student_id)
+    #return render(request, 'login/session.html')
