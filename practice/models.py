bf6aad18d590a55d915a538501b27e8ec1b22445
Iago <iago.correa@outlook.com>
2025-05-18 23:31:28 +0900

    Practice models

--- practice/models.py
+++ practice/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Audio(models.Model):
+    filename = models.CharField(max_length=255, unique=True)
+    transcript = models.TextField()
+
+    def __str__(self):
+        return f"{self.filename}: {self.transcript}"
+
+class Activity(models.Model):
+    session = models.ForeignKey("login.Session", on_delete=models.CASCADE, related_name="activities")
+    audio = models.ForeignKey(Audio, on_delete=models.CASCADE, related_name="activities")
