22aea80d6d34d062e14f75e3e7dd4a49f2de6959
Iago <iago.correa@outlook.com>
2025-06-02 07:39:02 +0900

    PostgreSQL

--- CAPT_Interface_python/settings.py
+++ CAPT_Interface_python/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'CAPT_Interface_python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432', cast=int),
     }
 }
 
