72891e45e60a7b5968acecc2eebeb5c365bf9b28
Iago <iago.correa@outlook.com>
2025-06-02 14:19:07 +0900

    Debuggin

--- CAPT_Interface_python/settings.py
+++ CAPT_Interface_python/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # For production:
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
