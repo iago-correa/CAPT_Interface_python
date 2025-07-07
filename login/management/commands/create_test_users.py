5f70108f55f8114c8a852324d6783fdca141d051
iago <iago@lee-lab.org>
2025-07-07 13:33:41 +0900

    Added test rater creation to create_test_users

--- login/management/commands/create_test_users.py
+++ login/management/commands/create_test_users.py
@@ -2,13 +2,14 @@ import csv
 import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from login.models import Student
-from django.contrib.auth.models import User
+from login.models import Student, Rater
 
 class Command(BaseCommand):
-    help = 'Create test users: 01010101 and 02020202'
-
+    help = 'Create test users: 01010101 and 02020202.'
     def handle(self, *args, **kwargs):
+
+        rater_id = os.environ.get('TEST_RATER_ID')
+        rater_password = os.environ.get('TEST_RATER_PASSWORD')
 
         try:
             Student.objects.get_or_create(
@@ -18,6 +19,10 @@ class Command(BaseCommand):
             Student.objects.get_or_create(
                 student_id='02020202',
                 control_group=False
+            )
+            Rater.objects.get_or_create(
+                rater_id=rater_id
+                password=rater_password
             )
         except Exception as e:
             self.stderr.write(self.style.ERROR(f"An error occurred during test users creation: {e}"))
